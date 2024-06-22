02ff06b4402f1676e989ea77ec6f6ce2324493e4
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2024-06-23 00:17:41 +0100

    Create SpeechSci.py

---
+++ SpeechSci.py
@@ -0,0 +1,26 @@
+# - Speech wave & frequency (measured in decibels):
+# source code:
+
+import SciPy
+import sys
+import _SpeechBackAudio
+
+
+def _TextTransition():
+    speech_Talk_Pattern_1 = 7
+    speech_Talk_Pattern_2 = 0.03
+
+
+def __ini__(self):
+    
+
+
+  
+
+
+
+
+
+
+
+
